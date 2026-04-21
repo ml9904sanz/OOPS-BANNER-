bd43ea1bd8ce67c5e690d04bf01aa60db216ad15
ml9904sanz <ml9904@srmist.edu.in>
2026-04-21 13:00:06 +0530

    w

---
+++ lvl1/VotingCheck.java
@@ -0,0 +1,29 @@
+package lvl1;
+
+import java.util.Scanner;
+
+public class VotingCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] ages = new int[10];
+
+        // Input
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            ages[i] = sc.nextInt();
+        }
+
+        // Processing
+        for (int i = 0; i < ages.length; i++) {
+            if (ages[i] < 0) {
+                System.out.println("Invalid age: " + ages[i]);
+            } else if (ages[i] >= 18) {
+                System.out.println("The student with age " + ages[i] + " can vote.");
+            } else {
+                System.out.println("The student with age " + ages[i] + " cannot vote.");
+            }
+        }
+
+        sc.close();
+    }
+}
